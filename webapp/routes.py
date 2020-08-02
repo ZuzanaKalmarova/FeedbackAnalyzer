b9800d8d0de885e552dab78757aa9aa7c9473382
ZuzanaKalmarova <zuzana_kalmarova@hotmail.sk>
2020-08-02 09:34:37 +0200

    removing int casting for rating when saving

--- webapp/routes.py
+++ webapp/routes.py
@@ -12,5 +12,5 @@ def process_feedback():
     rating = request.form.get('inlineRadioOptions')
     text = request.form.get('feedbackText')
     date = datetime.datetime.today()
-    mongo.db.feedbacks.insert({'rating': int(rating), 'feedback': text, 'date': date})
+    mongo.db.feedbacks.insert({'rating': rating, 'feedback': text, 'date': date})
     return '<h2>Thank you for your feedback</h2>'
